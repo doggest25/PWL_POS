6f7f0026f443145d521376824d37dc30609d5a69
Agusty Labdanayoga <yogest86@gmail.com>
2024-04-19 17:13:31 -0700

    js 9

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,14 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 
-class UserModel extends Model
-{
+class UserModel extends Model implements AuthenticatableContract
+{   
+    
+    use Authenticatable;
     use HasFactory;
-
+    
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
     /**
